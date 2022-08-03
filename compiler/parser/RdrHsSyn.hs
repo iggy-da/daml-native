684c02eb564de9917733b621b162043c7c37b0c1
otto-dev <ignatius.public@gmail.com>
2022-08-03 18:00:29 +1000

    Add build instructions

--- compiler/parser/RdrHsSyn.hs
+++ compiler/parser/RdrHsSyn.hs
@@ -2906,7 +2906,6 @@ mkTemplateInstanceDecl sharedBinds templateName conName ValidTemplate{..} =
   , ensureInstance
   , agreementInstance
   , archiveInstance
-  , mkInstance "HasCreate" $ mkPrimMethod "create" "UCreate"
   , mkInstance "HasFetch" $ mkPrimMethod "fetch" "UFetch"
   , mkInstance "HasToAnyTemplate" $ mkPrimMethod "_toAnyTemplate" "EToAnyTemplate"
   , mkInstance "HasFromAnyTemplate" $ mkPrimMethod "_fromAnyTemplate" "EFromAnyTemplate"
@@ -2975,7 +2974,6 @@ mkInterfaceInstanceDecl interfaceType interfacePrecondM =
   , mkInstance "HasTemplateTypeRep" $ mkPrimMethod "_templateTypeRep" "ETemplateTypeRep"
   , mkInstance "HasSignatory" $ mkPrimMethod "signatory" "ESignatoryInterface"
   , mkInstance "HasObserver" $ mkPrimMethod "observer" "EObserverInterface"
-  , mkInstance "HasCreate" $ mkPrimMethod "create" "UCreateInterface"
   , mkInstance "HasEnsure" $ mkMethod "ensure" [this] (fromMaybe mkTrue interfacePrecondM)
   , mkInstance "HasIsInterfaceType" $ mkMethod "_isInterfaceType" [proxy] mkTrue
   , mkInstance "Eq" $ mkPrimMethod "==" "BEEqual"
